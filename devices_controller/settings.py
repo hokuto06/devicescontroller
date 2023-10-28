3a859dd2d17a79c862da9203dbc09fbfb5ef8a48
hokuto06 <emartins.ecnex@gmail.com>
2023-10-28 02:49:09 -0300

    ALLOWED HOSTS

--- devices_controller/settings.py
+++ devices_controller/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-f%b6x-6$o-6b45_kzeft2-$4&3z$@e@(n=x!asnjf4fqyzspu*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.20.197.33']
 
 
 # Application definition
