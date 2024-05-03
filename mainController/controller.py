75ed1290bce151f8f874c660594b0bd2032f6efe
hokuto06 <emartins.ecnex@gmail.com>
2024-05-03 07:44:15 -0300

    controller rucus1

--- mainController/controller.py
+++ mainController/controller.py
@@ -126,6 +126,8 @@ def connectUnifi(ip_address, user, password, collection):
                     existing_device = None
                     device_data["_id"] = str(uuid.uuid4())
                     # Intenta actualizar el dispositivo existente o crear uno nuevo si no existe
+                except Exception as e:
+                    print(e)
                 device, created = Devices.objects.update_or_create(
                     ipAddress=ip_address,  # Condición de búsqueda: campo ipAddress
                     defaults=device_data  # Valores para actualizar o crear
